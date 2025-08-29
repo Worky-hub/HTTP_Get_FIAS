25c717a05e465ebfdcfcc15f039a95f69c4b112c
Worky-hub <crixcrax@rambler.ru>
2025-08-29 15:31:03 +0300

    Changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <winhttp.h>
 #include <string>
 
+#include <newtwork/uri.hpp>
+
 // #define BOOST_URL_HEADER_ONLY
 // #include <boost/url.hpp>
 
@@ -16,7 +18,7 @@ int main() {
     // const std::string& url = "https://nspd.gov.ru/api/geoportal/v2/search/geoportal?query=77%3A21%3A0151004%3A11&thematicSearchId=1";
     // wchar_t url[] = L"https://nspd.gov.ru/api/geoportal/v2/search/geoportal?query=77%3A21%3A0151004%3A11&thematicSearchId=1";
     std::string url = "https://nspd.gov.ru/api/geoportal/v2/search/geoportal?query=77%3A21%3A0151004%3A11&thematicSearchId=1";
-
+    network::uri theUrl (url);
     // auto parsedUrl = urls::parse_uri(url);
     // urls::url_view u = *parsedUrl;
     // std::cout << u.scheme();
@@ -25,7 +27,7 @@ int main() {
     // std::wstring wurl(url.begin(), url.end());
 
 
-    std::cout << "lol";
+    std::cout << "Scheme: " << theUrl.scheme();
 
 
 
